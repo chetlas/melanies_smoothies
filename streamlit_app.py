ee05061ebfe257760f723628e88d8475a63a7508
chetlas <162035931+chetlas@users.noreply.github.com>
2026-04-05 10:37:51 +0100

    Add API call to fetch fruit data and fix typo
    Fix typo in requests module and add API call to fetch fruit data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,6 @@ if ingredients_list:
     if time_to_insert:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!', icon="✅")
+import requests
+smoothiefroot_response = resquests.get("https://my.smoothiefroot.com/api/fruit/watefmelon")
+st.txt(smoothiefroot_response)
