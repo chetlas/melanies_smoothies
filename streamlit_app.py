a6c9805a9c6e56b17df90b7f4c6991852a8574e3
chetlas <162035931+chetlas@users.noreply.github.com>
2026-04-05 10:56:15 +0100

    Add requests import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 # Write directly to the app.
 st.title( ":cup_with_straw: Customize your smoothie!:cup_with_straw:")
 st.write(
@@ -36,6 +37,6 @@ if ingredients_list:
     if time_to_insert:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!', icon="✅")
-import requests  
+  
 
 
